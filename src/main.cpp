d8e35494365e98ea3080bd50d04081e09bd41d31
MRIDUL TYAGI <mridultyagi@china.local>
2025-11-01 01:36:48 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv){
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -49,11 +48,17 @@ int main(int argc, char **argv) {
 
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
-
-  
   accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
   
+  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr ,(socketlen_t*)&client_addr_len);
+  if(client_fd<0){
+    std ::cerr << "accept failed\n" ;
+  }
+  else{
+    std :: string response = "+PONG\r\n" ;
+    send(client_fd , response.c_str(), response.length(), 0 ) ;
+  }
   close(server_fd);
 
   return 0;
